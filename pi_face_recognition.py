e0ca15e64d83c9c11aab1ff92aa657680ac25762
diogoshiroma <diogoshiroma@hotmail.com>
2020-11-17 11:08:21 -0300

    Add liveness detection

--- pi_face_recognition.py
+++ pi_face_recognition.py
@@ -1,9 +1,15 @@
 # USAGE
-# python pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
+"""
+python3 pi_face_recognition.py \
+	--model liveness_detection/liveness.model --le liveness_detection/le.pickle --detector liveness_detection/face_detector
+""" 
 
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
 from rekognition import *
 import face_recognition
 import argparse
@@ -11,20 +17,31 @@ import imutils
 import pickle
 import time
 import cv2
+import os
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--cascade", required=True,
-	help = "path to where the face cascade resides")
-ap.add_argument("-e", "--encodings", required=True,
-	help="path to serialized db of facial encodings")
+ap.add_argument("-m", "--model", type=str, required=True,
+	help="path to trained model")
+ap.add_argument("-l", "--le", type=str, required=True,
+	help="path to label encoder")
+ap.add_argument("-d", "--detector", type=str, required=True,
+	help="path to OpenCV's deep learning face detector")
+ap.add_argument("-c", "--confidence", type=float, default=0.9,
+	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
-# load the known faces and embeddings along with OpenCV's Haar
-# cascade for face detection
-print("[INFO] loading encodings + face detector...")
-data = pickle.loads(open(args["encodings"], "rb").read())
-detector = cv2.CascadeClassifier(args["cascade"])
+# load our serialized face detector from disk
+print("[INFO] loading face detector...")
+protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+modelPath = os.path.sep.join([args["detector"],
+	"res10_300x300_ssd_iter_140000.caffemodel"])
+net = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+# load the liveness detector model and label encoder from disk
+print("[INFO] loading liveness detector...")
+model = load_model(args["model"])
+le = pickle.loads(open(args["le"], "rb").read())
 
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
@@ -32,10 +49,9 @@ vs = VideoStream(src=0).start()
 # vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 
-# start the FPS counter
-fps = FPS().start()
-
 aws_flag = False
+COUNTER = 0
+FRAMES_TO_DETECT_LIVENESS = 50
 
 # loop over frames from the video file stream
 while True:
@@ -43,64 +59,83 @@ while True:
 	# to 500px (to speedup processing)
 	frame = vs.read()
 	frame = imutils.resize(frame, width=500)
-	
-	# convert the input frame from (1) BGR to grayscale (for face
-	# detection) and (2) from BGR to RGB (for face recognition)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE)
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+		(300, 300), (104.0, 177.0, 123.0))
 
-	# OpenCV returns bounding box coordinates in (x, y, w, h) order
-	# but we need them in (top, right, bottom, left) order, so we
-	# need to do a bit of reordering
-	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
 
-	# compute the facial embeddings for each face bounding box
-	encodings = face_recognition.face_encodings(rgb, boxes)
-	names = []
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with the
+		# prediction
+		confidence = detections[0, 0, i, 2]
 
-	# loop over the facial embeddings
-	# for encoding in encodings:
-	# 	# attempt to match each face in the input image to our known
-	# 	# encodings
-	# 	matches = face_recognition.compare_faces(data["encodings"],
-	# 		encoding)
-	# 	name = "Unknown"
+		# filter out weak detections
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the face and extract the face ROI
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
 
-	# 	# check to see if we have found a match
-	# 	if True in matches:
-	# 		# find the indexes of all matched faces then initialize a
-	# 		# dictionary to count the total number of times each face
-	# 		# was matched
-	# 		matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-	# 		counts = {}
+			# ensure the detected bounding box does fall outside the
+			# dimensions of the frame
+			startX = max(0, startX)
+			startY = max(0, startY)
+			endX = min(w, endX)
+			endY = min(h, endY)
 
-	# 		# loop over the matched indexes and maintain a count for
-	# 		# each recognized face face
-	# 		for i in matchedIdxs:
-	# 			name = data["names"][i]
-	# 			counts[name] = counts.get(name, 0) + 1
+			# extract the face ROI and then preproces it in the exact
+			# same manner as our training data
+			face = frame[startY:endY, startX:endX]
+			try:
+				face = cv2.resize(face, (32, 32))
+				face = face.astype("float") / 255.0
+				face = img_to_array(face)
+				face = np.expand_dims(face, axis=0)
+			except cv2.error as e:
+				continue
 
-	# 		# determine the recognized face with the largest number
-	# 		# of votes (note: in the event of an unlikely tie Python
-	# 		# will select first entry in the dictionary)
-	# 		name = max(counts, key=counts.get)
-		
-	# 	# update the list of names
-	# 	names.append(name)
+			# pass the face ROI through the trained liveness detector
+			# model to determine if the face is "real" or "fake"
+			preds = model.predict(face)[0]
+			j = np.argmax(preds)
+			label = le.classes_[j]
 
-	# loop over the recognized faces
-	# for ((top, right, bottom, left), name) in zip(boxes, names):
-	# 	# draw the predicted face name on the image
-	# 	cv2.rectangle(frame, (left, top), (right, bottom),
-	# 		(0, 255, 0), 2)
-	# 	y = top - 15 if top - 15 > 15 else top + 15
-	# 	cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-	# 		0.75, (0, 255, 0), 2)
+			if label == "real":
+				COUNTER += 1
+			else:
+				COUNTER = 0
+
+			# draw the label and bounding box on the frame
+			label = "{}: {:.4f}".format(label, preds[j])
+			cv2.putText(frame, label, (startX, startY - 10),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				(0, 0, 255), 2)
+			
+			if COUNTER >= FRAMES_TO_DETECT_LIVENESS:
+				print("Liveness detected!")
+				if not aws_flag:
+					print('Analysing face...')
+					faceMatches = aws_rekognition()
+					print ('Matching faces')
+					if faceMatches:
+						for match in faceMatches:
+								# print ('FaceId:' + match['Face']['FaceId'])
+								print ('FaceId:' + match['Face']['ExternalImageId'])
+								print ('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
+								print ('LOCKER UNLOCKED!\n')
+								aws_flag = True
+					else:
+						print('No match!\n')
+
+				COUNTER = 0
 
 	# display the image to our screen
 	cv2.imshow("Frame", frame)
@@ -110,29 +145,6 @@ while True:
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
-
-	# update the FPS counter
-	fps.update()
-
-	if len(rects) and not aws_flag:
-		print('Analisando face...')
-		faceMatches = aws_rekognition()
-		print ('Matching faces')
-		if faceMatches:
-			for match in faceMatches:
-					# print ('FaceId:' + match['Face']['FaceId'])
-					print ('FaceId:' + match['Face']['ExternalImageId'])
-					print ('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
-					aws_flag = True
-		else:
-			print('No match!')
-
-	# time.sleep(1)
-
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
 # do a bit of cleanup
 vs.stop()
